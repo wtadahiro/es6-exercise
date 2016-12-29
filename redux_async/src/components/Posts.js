68ed5572af0ef02dc4df401f746737dcc2dbce42
tadahiro.wada <tadahiro.wada@d8aspring.com>
2016-12-29 18:09:14 +0900

    fixed :bug:

---
+++ redux_async/src/components/Posts.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+
+const Posts = ({posts}) => (
+    <ul>
+        {posts.map((post, i) => 
+            <li key={i}>{post.title}</li>
+        )}
+    </ul>
+)
+
+
+Posts.propTypes = {
+    posts: PropTypes.array.isRequired
+}
+
+export default Posts;
